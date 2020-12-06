8486222cdbf25374f26e0b4821d0948e6ece0164
lpilas24 <leiva20152@gmail.com>
2020-12-06 00:11:28 -0500

    Se añadió las views de reset password

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,3 +135,10 @@ LOGIN_REDIRECT_URL = 'blog-home'
 #redireccionamossi es que ingresan a profile y no estan loggeados
 LOGIN_URL = 'login'
 
+#datos para el envio de email luego de confirmar reseteo de email y password
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER') #crear estas variables de environment
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -27,6 +27,26 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'), #ruta de userprofile
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),#mapeo haci registrar usuario
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/', 
+    auth_views.PasswordResetView.as_view(#3 rutas para el reinicio de contrase;as
+        template_name='users/password_reset.html'
+        ), 
+        name='password_reset'),
+    path('password-reset/done/', 
+    auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'
+        ), 
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', #parametros que django pide obligatoriamente ( id del usuario encriptado en base 64/token que chequea si el password es valido )
+    auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'
+        ), 
+        name='password_reset_confirm'),
+    path('password-reset-complete/', #parametros que django pide obligatoriamente ( id del usuario encriptado en base 64/token que chequea si el password es valido )
+    auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'
+        ), 
+        name='password_reset_complete'),
     path('', include('blog.urls')), #mapeo hacia las urls de blog ruta /blog
 ]
 
